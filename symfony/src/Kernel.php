6bce900eba360a177baa4a74a7c1f3a7d9d9c0d3
Stefan <stefan@stefan-sander.online>
2026-02-20 12:16:52 +0100

    Import attribute routes in Kernel

--- symfony/src/Kernel.php
+++ symfony/src/Kernel.php
@@ -3,9 +3,18 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        $routes->import('../config/routes/*.yaml');
+        $routes->import('../config/routes/{env}/*.yaml');
+        $routes->import('../src/Controller/', 'attribute');
+        $routes->import('@SnokeWsBundle/Controller/', 'attribute');
+    }
 }
